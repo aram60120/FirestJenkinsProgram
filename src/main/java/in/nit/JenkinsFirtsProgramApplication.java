377781704777d9b835cfdc95aae3f5b071ee021b
aram60120 <aram60120@gmail.com>
2021-01-10 12:22:57 +0530

    HIS-134

---
+++ src/main/java/in/nit/JenkinsFirtsProgramApplication.java
@@ -0,0 +1,15 @@
+package in.nit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsFirtsProgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsFirtsProgramApplication.class, args);
+	System.out.println("Hellow WOrld");
+	
+	}
+
+}
